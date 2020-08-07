0d5512b63343a32586a5d0cd1faf4e53763b87b8
SkyLightQP <combbm@naver.com>
2020-08-07 15:53:52 +0900

    feat: implement NoticeBar component

---
+++ src/components/NoticeBar.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  background-color: #f6f6f6;
+  height: 45%;
+  border-radius: 30px;
+  font-family: "Gothic A1", sans-serif;
+  font-size: 0.8rem;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  align-self: center;
+
+  box-shadow: 0px 2px 20px #dedede;
+`;
+
+const NoticeBar: React.FC = ({ children }) => {
+  return <Wrapper>{children}</Wrapper>;
+};
+
+export default NoticeBar;
